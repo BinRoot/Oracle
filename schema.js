c706bb751c10dc60d450e0a86f3620b1af033064
BinRoot <nick722@gmail.com>
2013-02-03 14:55:32 -0500

    added schema

---
+++ schema.js
@@ -0,0 +1,20 @@
+
+// Schema for 'Code' Collection
+var codeItem =
+    { 
+	type: "bubble sort", // category of code
+	author: "binroot", // original author
+	language: "java", // programming language
+	code: "code", // implementation of code
+	timestamp: "Jan 31, 2013" // time created
+    }
+
+
+// Schema for 'User' Collection
+var userItem = 
+    {
+	name: "phiV",
+	id: "lk3jd3ljflq",
+	timestamp: "Jan 30, 2013",
+	history: [{action: "edit", name:"Bubble sort", lang:"Java", cid:"3x42sw"}]
+    }
