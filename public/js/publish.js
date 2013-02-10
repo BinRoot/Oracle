a119ccb2cf2439db886c450f8076b8930dd15aff
BinRoot <nick722@gmail.com>
2013-02-10 16:18:23 -0500

    updated gist part

--- public/js/publish.js
+++ public/js/publish.js
@@ -4,3 +4,17 @@ var upload = function(fpfile){
 		editor.setValue(data);
 	});
 };
+
+$('#gist').click(function() {
+    var children = $('#options').children();
+    clearAllActiveChildren(children);
+    $('#gistLI').addClass('active');
+});
+
+
+function clearAllActiveChildren (children) {
+    for(var i=0; i<children.length; i++) {
+	var v = children[i]
+	$(v).removeClass('active');
+    }
+}
