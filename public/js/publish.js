459cdfc8abc22042b56e3e1bdd0343ce6e81d729
Jasdev Singh <jasdevs@gmail.com>
2013-02-10 16:17:52 -0500

    refactor fp logic

--- public/js/publish.js
+++ public/js/publish.js
@@ -1,6 +1,18 @@
-var upload = function(fpfile){
-	console.log(fpfile);
-	filepicker.read(fpfile, function(data){
-		editor.setValue(data);
-	});
-};
+filepicker.setKey('AoO2NYenFQq2z9yVBtOEKz');
+
+$('#upload').click(
+	function(){
+		console.log("Upload clicked");
+		filepicker.pick(
+		{
+			services: ['GITHUB', 'COMPUTER', 'GOOGLE_DRIVE', 'URL'],
+			maxSize: 100*1024
+		},
+		function(fpfile){
+			filepicker.read(fpfile, function(data){
+				editor.setValue(data);
+				editor.setReadOnly(true);
+			});
+		});
+	}
+);
