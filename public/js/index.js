a22d5e7891de27504739bdd2c2ddf5eb4fa2e49a
BinRoot <nick722@gmail.com>
2013-02-22 00:42:18 -0500

    fixed merge issues

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,3 @@
-$("#searchInput").typeahead({
-    source: function(query, process) {
-	$.get('/peek', { q: query }, function(data) {
-	    var d = JSON.parse(data);
-	    var types = [];		
-	    for(var i=0; i<d.length; i++) {
-		types.push(d[i].type + " ("+d[i].sum+")");
-	    }
-	    process(types);
-	});
-    },
-    updater: function (item) {
-	var splits = item.split(' ');
-	item = "";
-	for(var i=0; i<splits.length-1; i++) {
-	    item = item + " " + splits[i];
-	}
-        return item;
-    },
-    minLength: 1
-});
-
 $('.search-bar input').focus(function() {
 	$(this).parent().addClass('focus');
 });
