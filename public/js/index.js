01add4c5a6397f9fbf126611db3d592ff1f4ca2b
BinRoot <nick722@gmail.com>
2013-03-15 04:07:43 -0400

    search functional

--- public/js/index.js
+++ public/js/index.js
@@ -13,10 +13,106 @@ $('#find-button').click(function() {
 
     $.get(searchURL, function(data) {
 	console.log(data);
-	$('#search-results').append(
-	    $('<div>').attr('class', 'result').append(
-		"hi"
+
+	data = JSON.parse(data);
+
+	var langFreq = [];
+
+	$('#search-results').empty();
+
+	for(var i=0; i<data.docs.length; i++) {
+	    var docItem = data.docs[i];
+	    
+	    var voteStr = docItem.votes>1 ? "votes" : "vote";
+
+	    // check if exists
+	    var found = false;
+	    for(var j=0; j<langFreq.length; j++) {
+		if(langFreq[j].lang == docItem.lang) { // found it, increase it by 1
+		    langFreq[j].value++;
+		    found = true;
+		    break;
+		}
+	    }
+	    if(!found) {
+		langFreq.push({lang: docItem.lang, value: 1});
+	    }
+	    
+	    $('#search-results').append(
+		$('<div>').addClass('result').append(
+		    $('<ul>').addClass('inline-block').append(
+			$('<li>').append(
+			    $('<div>').addClass('avatars').append(
+				$('<div>').addClass('circular').css('background', 'url(http://gravatar.com/avatar/7bb3f29d02f3cb9e350616b849452b7b?s=100) no-repeat;')
+			    ).append(
+				$('<div>').addClass('votes').append(
+				    $('<span>').addClass('votes-num').append(
+					docItem.votes
+				    ).append(
+					$('<span>').addClass('votes-votes').append(
+					    voteStr
+					)	
+				    )
+				)
+			    )
+			)
+		    ).append(
+			$('<li>').append(
+			    $('<div>').addClass('meta').append(
+				$('<div>').append(
+				    $('<span>').append(
+					"By "+docItem.uname
+				    ).append(
+					$('<span>').addClass('comments').append(
+					    "0 comments"
+					)
+				    )
+				).append(
+				    $('<div>').addClass('snippet').append(
+					"<pre><code>"+ docItem.code +"</code></pre>"
+				    )
+				)
+
+			    )
+			
+			)
+		    )
+		)
 	    )
-	);
+	    
+
+	}
+
+	langFreq = langFreq.sort(function(a,b) {return b.value-a.value});
+	console.log('sorted lang freq: '+JSON.stringify(langFreq));
+	
+	$('#language-results').empty();
+
+	for(var i=0; i<langFreq.length; i++) {
+
+	    var langClass = i==0 ? 'lang-item lang-active' : 'lang-item';
+
+	    $('#language-results').append(
+		$('<div>').addClass(langClass).append(
+		    $('<span>').append(
+			titleCase(langFreq[i].lang) + " "
+		    ).append(
+			$('<span>').addClass('lang-number').append(
+			    langFreq[i].value
+			)
+		    )
+		)
+	    );
+	    
+	    if(i < langFreq.length-1) {
+		$('#language-results').append("<hr>");
+	    }
+	}
+	    
+
     });
 });
+
+function titleCase(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
