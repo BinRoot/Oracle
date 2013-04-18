d4351671c7686105df820a105349db774d5ff6a1
BinRoot <nick722@gmail.com>
2013-04-18 00:56:42 -0400

    fixed typeahead for /publish

--- public/js/publish.js
+++ public/js/publish.js
@@ -1,26 +1,3 @@
-$("#type").typeahead({
-    source: function(query, process) {
-	$.get('/peek', { q: query }, function(data) {
-	    var d = JSON.parse(data);
-	    var types = [];
-	    for(var i=0; i<d.length; i++) {
-		types.push(d[i].type + " ("+d[i].sum+")");
-	    }
-	    process(types);
-	});
-    },
-    updater: function (item) {
-	var splits = item.split(' ');
-	item = "";
-	for(var i=0; i<splits.length-1; i++) {
-	    item = item + " " + splits[i];
-	}
-        return item;
-    },
-    minLength: 1
-});
-
-
 $('#type').typeahead(
     {
         source: function(query, process) {
@@ -43,7 +20,7 @@ $('#type').typeahead(
         },
         updater: function(item) {
             var splits = item.split(" ");
-            varoutStr = [];
+            var outStr = [];
 	        for(var i=0; i<splits.length-1; i++) {
                 outStr.push(splits[i]);
             }
