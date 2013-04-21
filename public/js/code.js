0cc81826c414d6cb9272690b6e3652f84646061a
BinRoot <nick722@gmail.com>
2013-04-21 16:54:27 -0400

    voting implemented

--- public/js/code.js
+++ public/js/code.js
@@ -1,3 +1,18 @@
+$(document).ready(function() {
+    var userExtra = JSON.parse($('#userExtraHolder').html());
+    var code = JSON.parse($('#codeHolder').html()).docs[0];
+
+    console.log(JSON.stringify(userExtra));
+    console.log(JSON.stringify(code));
+    
+    if(userExtra && userExtra.votes) {
+	if(userExtra.votes.indexOf(code.id) != -1) {
+	    $('#upvote').addClass('upvoted');
+	    $('#upvote').click();
+	}
+    }
+});
+
 hljs.initHighlightingOnLoad();
 
 $('#find-button').click(function() {
@@ -18,6 +33,31 @@ function search(searchStr) {
 }
 
 $('#upvote').click(function(){
-  $(this).effect("bounce", {distance: 5} , 500).animate({color: "#ff5700"}, 200 );
+    if( $(this).hasClass('upvoted') ) {
+	console.log('already upvoted, doing nothing');
+	$(this).effect("bounce", {distance: 5} , 500).animate({color: "#ff5700"}, 200 );
+    }
+    else {
+	console.log('upvoting..');
+
+	var code = JSON.parse($('#codeHolder').html()).docs[0];
+	var userExtra = JSON.parse($('#userExtraHolder').html());
+
+	// if user signed in
+	if(userExtra) {
+	    $.ajax({
+		url: "/api/vote?cid="+code.id,
+	    }).done(function ( data ) {
+		console.log('done voting: '+data);
+		$('#upvote').effect("bounce", {distance: 5} , 500).animate({color: "#ff5700"}, 200 );
+		$('#upvote').addClass('upvoted');
+	    });
+	}
+	else {
+	    console.log('user not signed in');
+	    window.open('/auth/google');
+	}
+
+    }
 });
 
