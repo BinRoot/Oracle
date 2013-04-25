9543be5e84a05dc7b7c8d0185774f7556675853d
BinRoot <nick722@gmail.com>
2013-04-25 16:12:24 -0400

    fixed some mappings.

--- public/js/publish.js
+++ public/js/publish.js
@@ -52,7 +52,7 @@ $('#upload').click(function() {
 	    filepicker.read(fpfile, function(data){
                 var extension = '.' + fpfile.filename.split('.').pop();
 		console.log('filepicker extension: '+extension);
-                updateCode(aceLanguages[extension], data);
+                updateCode(fileToAceMap[extension], fileToDbMap[extension], data);
 	    });
 	});
 });
@@ -71,7 +71,9 @@ $('#languages li').click(function() {
     allowLangGuessing = false;
     var lang = $(this).text();
     $('#selectedLanguage').html( lang + ' <span class="caret"></span>');
-    editor.getSession().setMode("ace/mode/" + lang.toLowerCase());
+
+    editor.getSession().setMode("ace/mode/" + displayToAceMap[lang.toLowerCase()]);
+
 });
 
 $('#gistOK').click(function() {
@@ -100,7 +102,7 @@ $('#gistOK').click(function() {
 
                 console.log(lang);
 
-                updateCode(gistLanguages[lang], code);
+                updateCode(displayToAceMap[lang], displayToDbMap[lang], code);
                 var children = $('#options').children();
                 clearAllActiveChildren(children);
                 $('#gistLI').addClass('active');
@@ -117,25 +119,26 @@ $('#gistOK').click(function() {
     }
 });
 
-function updateCode(lang, code) {
+function updateCode(aceLang, dbLang, code) {
     allowLangGuessing = false;
 
     editor.setValue(code);
 
-    console.log('updateCode, lang: '+lang);
+    console.log('updateCode, db-lang: '+dbLang);
 
-    if(typeof lang === 'undefined'){
+    if(typeof aceLang === 'undefined'){
         editor.getSession().setMode(null);
-        $('#selectedLanguage').html( "other" + ' <span class="caret"></span>');
+//        $('#selectedLanguage').html( "other" + ' <span class="caret"></span>');
     }
     else{
-        console.log("Setting code to language " + lang);
-        editor.getSession().setMode("ace/mode/" + lang);
-        $('#selectedLanguage').html( lang.charAt(0).toUpperCase() + lang.slice(1) + ' <span class="caret"></span>');
+        console.log("Setting code to language " + aceLang);
+        editor.getSession().setMode("ace/mode/" + aceLang);
+
+        $('#selectedLanguage').html( dbToDisplayMap[dbLang] + ' <span class="caret"></span>');
     }
 }
 
-
+// TODO add highlight mapping!!
 editor.getSession().on('change', function(e) {
     if(allowLangGuessing) {
 	var code = editor.getValue();
@@ -149,9 +152,10 @@ editor.getSession().on('change', function(e) {
 	console.log('-');
 
 	var lang = out.language;
+	
 	if(lang) {
-	    $('#selectedLanguage').html( lang.charAt(0).toUpperCase() + lang.slice(1) + ' <span class="caret"></span>');
-	    editor.getSession().setMode("ace/mode/" + lang);
+	    $('#selectedLanguage').html( highlightToDisplayMap[lang] + ' <span class="caret"></span>');
+	    editor.getSession().setMode("ace/mode/" + highlightToAceMap[lang]);
 	}
     }
 });
@@ -209,13 +213,14 @@ function search(str) {
 
 //Add mimetype and ace language mappings here
 //Language listing here https://github.com/ajaxorg/ace/tree/master/lib/ace/mode
-var aceLanguages = {
+var fileToAceMap = {
                     ".java":"java",
-                    ".py":"python",
                     ".js":"javascript",
-                    ".rb":"ruby",
+                    ".py":"python",
                     ".hs":"haskell",
+                    ".rb":"ruby",
                     ".cpp":"c_cpp",
+                    ".c":"c_cpp",
                     ".sh":"shell",
                     ".php": "php",
                     ".pl":"perl",
@@ -224,23 +229,123 @@ var aceLanguages = {
                     ".coffee": "coffee",
                     ".go":"golang",
                     ".scm":"scheme",
-                    ".ss":"scheme"
+                    ".ss":"scheme",
+                    ".html":"html",
+                    ".css":"css",
+                    ".tex":"tex"
                 };
 
-var gistLanguages = {
+var fileToDbMap = {
+                    ".java":"java",
+                    ".js":"javascript",
+                    ".py":"python",
+                    ".hs":"haskell",
+                    ".rb":"ruby",
+                    ".cpp":"c_cpp",
+                    ".c":"c", //This is the only difference
+                    ".sh":"shell",
+                    ".php": "php",
+                    ".pl":"perl",
+                    ".m": "objectivec",
+                    ".cs":"csharp",
+                    ".coffee": "coffee",
+                    ".go":"golang",
+                    ".scm":"scheme",
+                    ".ss":"scheme",
+                    ".html":"html",
+                    ".css":"css",
+                    ".tex":"tex"
+                };
+
+
+// mapping from drop-down displayed language --> ace lang
+var displayToAceMap = {
                     "Java": "java",
                     "JavaScript":"javascript",
                     "Python" : "python",
-                    "Haskell":"haskell",
+                    "Haskell":"text",
                     "Ruby":"ruby",
                     "C++":"c_cpp",
                     "C":"c_cpp",
-                    "Shell":"shell",
+                    "Shell":"sh",
                     "PHP":"php",
                     "Perl":"perl",
                     "Objective-C": "objectivec",
                     "C#":"csharp",
                     "CoffeeScript":"coffee",
                     "Go":"golang",
-                    "Scheme":"scheme"
+                    "Scheme":"text",
+                    "HTML":"html",
+                    "CSS":"css",
+                    "Matlab":"text",
+                    "Tex":"tex"
                 };
+
+var highlightToAceMap = {
+    "bash": "shell",
+    "cs": "csharp",
+    "ruby": "ruby",
+    "javascript": "javascript",
+    "css": "css",
+    "xml": "html",
+    "java": "java",
+    "php": "php",
+    "haskell": "text",
+    "python": "python",
+    "tex": "tex",
+    "perl": "perl",
+    "objectivec": "objectivec",
+    "coffeescript": "coffee",
+    "cpp": "cpp",
+    "matlab": "text",
+    "go": "go",
+}
+
+var highlightToDisplayMap = {
+    "bash": "Shell",
+    "cs": "C#",
+    "ruby": "Ruby",
+    "javascript": "JavaScript",
+    "css": "CSS",
+    "xml": "HTML",
+    "java": "Java",
+    "php": "PHP",
+    "haskell": "Haskell",
+    "python": "Python",
+    "tex": "Tex",
+    "perl": "Perl",
+    "objectivec": "Objective-C",
+    "coffeescript": "CoffeeScript",
+    "cpp": "C++",
+    "matlab": "Matlab",
+    "go": "Go",
+}
+
+
+var displayToDbMap = {
+                    "Java": "java",
+                    "JavaScript":"javascript",
+                    "Python" : "python",
+                    "Haskell":"haskell",
+                    "Ruby":"ruby",
+                    "C++":"c_cpp",
+                    "C":"c", // This is the only diff
+                    "Shell":"sh",
+                    "PHP":"php",
+                    "Perl":"perl",
+                    "Objective-C": "objectivec",
+                    "C#":"csharp",
+                    "CoffeeScript":"coffee",
+                    "Go":"golang",
+                    "Scheme":"text",
+                    "HTML":"html",
+                    "CSS":"css",
+                    "Matlab":"text",
+                    "Tex":"tex"
+                };
+
+var dbToDisplayMap = {};
+for(var disp in displayToDbMap){
+    dbToDisplayMap[displayToDbMap[disp]] = disp;
+}
+
